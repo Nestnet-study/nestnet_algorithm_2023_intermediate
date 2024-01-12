0e8f45beee909a63446e4f20d294120ec1077629
todus26 <jungsae2248@naver.com>
2024-01-13 00:43:07 +0900

    (#10) [1주차] 정새연

---
+++ JungSaeYeon/week_1/Boj10988.c
@@ -0,0 +1,16 @@
+////B.10988 팰린드롬인지 확인하기
+//
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//    char word[101];
+//    scanf("%s", word);
+//    for (int i = 0; i <= (strlen(word)/2); i++) {
+//        if (word[i] != word[strlen(word) - i - 1]) {
+//            printf("0");
+//            return 0;
+//        }
+//    }
+//    printf("1");
+//    return 0;
+//}
